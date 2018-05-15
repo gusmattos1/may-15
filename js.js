3ee662b7ec4c58b97a59fac5836f41c09eb7d085
gusmattos1 <gusmattos1@gmail.com>
2018-05-15 13:27:02 -0400

    assgim+nment done

---
+++ js.js
@@ -0,0 +1,45 @@
+project = {
+  committee: ["Stella", "Salma", "Kai"],
+  title: "Very Important Project",
+  dueDate : "December 14, 2019",
+  id: "3284",
+  steps: [
+    {description: "conduct interviews",
+     dueDate: "August 1, 2018"
+    },
+    {description: "code of conduct",
+     dueDate: "January 1, 2018"
+    },
+    {description: "compile results",
+     dueDate: "November 10, 2018"
+    },
+    {description: "version 1",
+     dueDate: "January 15, 2019"
+    },
+    {description: "revisions",
+     dueDate: "March 30, 2019"
+    },
+    {description: "version 2",
+     dueDate: "July 12, 2019"
+    },
+    {description: "final edit",
+     dueDate: "October 1, 2019"
+    },
+    {description: "final version",
+     dueDate: "November 20, 2019"
+    },
+    {description: "Wrap up",
+     dueDate: "December 1, 2019"
+    }
+  ]
+}
+index = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+array = [0, 1, 2, 0, 1, 2, 6, 7, 8]
+
+project['steps'].forEach(function(elem, index) {
+
+  elem['person']=project['committee'][(index%3)]
+
+});
+
+console.log(project);
